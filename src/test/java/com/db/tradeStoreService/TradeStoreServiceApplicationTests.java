d359403a0a96b2f18bbb64d90d88b17c6c17e0b7
LENOVO <coderakash89@gmail.com>
2021-08-10 00:20:05 +0530

    DB Trade Store Service

---
+++ src/test/java/com/db/tradeStoreService/TradeStoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.tradeStoreService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeStoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
